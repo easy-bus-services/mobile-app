85d726e2d1397415e4c750d2b5feeda4dc878d02
Yellamandarao Vemula <yalamanda118@gmail.com>
2025-09-18 00:12:59 +0530

    customer and super admin apps created

---
+++ customer/android/app/src/main/kotlin/com/easybusbooking/customer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.easybusbooking.customer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ super_admin/android/app/src/main/kotlin/com/easybusbooking/super_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.easybusbooking.super_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
